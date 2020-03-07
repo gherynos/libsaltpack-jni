71a6cb1e03d96375165471eba30b54d3fa12cb27
Gherynos <gherynos@naes.co>
2020-03-07 18:03:46 +0000

    Updated Android compile script to support latest NDK

--- android/compile.sh
+++ android/compile.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-NDK_VER=r14b
-LIBSODIUM_VER=stable
+NDK_VER=r20b
+NDK_PLATFORM_NUM=21
+export NDK_PLATFORM=android-$NDK_PLATFORM_NUM
+LIBSODIUM_VER=1.0.18
 LIBGMP_VER=6.1.2
 MSGPACK_VER=2.1.5
-LIBSALTPACK_VER=0.2.2
+#LIBSALTPACK_VER=0.2.3
+LIBSALTPACK_VER=master
 
 PT=/opt/libsaltpack-jni/android/tmp
-cd $PT
+cd $PT || exit
 
 apt-get update
 apt-get install -y git zip unzip wget build-essential autoconf libtool python
@@ -15,8 +18,8 @@ apt-get install -y git zip unzip wget build-essential autoconf libtool python
 # Android NDK
 echo "Android NDK"
 ARCH=$(uname -m)
-if [ $ARCH != "x86_64" ]; then
-  $ARCH = "x86"
+if [ "$ARCH" != "x86_64" ]; then
+  ARCH="x86"
 fi
 if [ ! -e "$PT/android-ndk-$NDK_VER" ]
 then
@@ -34,19 +37,16 @@ if [ ! -e "$PT/libsodium-$LIBSODIUM_VER" ]
 then
     if [ ! -e "$PT/libsodium-$LIBSODIUM_VER.tar.gz" ]
     then
-        wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz -O libsodium-$LIBSODIUM_VER.tar.gz
+        wget https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VER-RELEASE/libsodium-$LIBSODIUM_VER.tar.gz -O libsodium-$LIBSODIUM_VER.tar.gz
     fi
     tar -xvzf libsodium-$LIBSODIUM_VER.tar.gz
-    cd libsodium-$LIBSODIUM_VER
+    cd libsodium-$LIBSODIUM_VER || exit
     ./autogen.sh
-    ./dist-build/android-arm.sh
     ./dist-build/android-armv7-a.sh
     ./dist-build/android-armv8-a.sh
-    ./dist-build/android-mips32.sh
-    ./dist-build/android-mips64.sh
     ./dist-build/android-x86.sh
     ./dist-build/android-x86_64.sh
-    cd $PT
+    cd $PT || exit
 fi
 export LIBSODIUM_PATH=$PT/libsodium-$LIBSODIUM_VER
 
@@ -59,33 +59,41 @@ then
         wget https://gmplib.org/download/gmp/gmp-$LIBGMP_VER.tar.bz2
     fi
     tar -xvjf gmp-$LIBGMP_VER.tar.bz2
-    cd gmp-$LIBGMP_VER
-    ARCH=("armv6" "armv7-a" "armv8-a" "i686" "mips32" "mips64r6" "westmere")
-    HOST=("arm-linux-androideabi" "arm-linux-androideabi" "aarch64-linux-android" "i686-linux-android" "mipsel-linux-android" "mips64el-linux-android" "x86_64-linux-android")
-    for i in `seq 0 6`;
+    cd gmp-$LIBGMP_VER || exit
+    ARCH=("armv7-a" "armv8-a" "i686" "westmere")
+    HOST=("armv7a-linux-androideabi" "aarch64-linux-android" "i686-linux-android" "x86_64-linux-android")
+    for i in $(seq 0 3);
     do
         arch=${ARCH[i]}
         host=${HOST[i]}
+        if [ "$host" == "armv7a-linux-androideabi" ]
+        then
+            host_ar="arm-linux-androideabi"
+        else
+            host_ar="$host"
+        fi
+        toolchain=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
 
         mkdir android-$arch
-        cd android-$arch
+        cd android-$arch || exit
 
-        PATH=$PT/libsodium-$LIBSODIUM_VER/android-toolchain-$arch/bin:$PATH
-        SYSROOT=$PT/libsodium-$LIBSODIUM_VER/android-toolchain-$arch/sysroot
-        export CC="$host-clang --sysroot $SYSROOT"
-        export CXX="$host-clang++ --sysroot $SYSROOT"
-        export AR="$PT/libsodium-$LIBSODIUM_VER/android-toolchain-$arch/bin/$host-ar"
-        export RANLIB="$PT/libsodium-$LIBSODIUM_VER/android-toolchain-$arch/bin/$host-ranlib"
+        PATH=$toolchain/bin:$PATH
+        SYSROOT=$toolchain/sysroot
+        export CC="$host$NDK_PLATFORM_NUM-clang --sysroot $SYSROOT"
+        export CXX="$host$NDK_PLATFORM_NUM-clang++ --sysroot $SYSROOT"
+        export AR="$toolchain/bin/$host_ar-ar"
+        export RANLIB="$toolchain/bin/$host_ar-ranlib"
 
-        $PT/gmp-$LIBGMP_VER/./configure --host=$host --disable-assembly --enable-cxx --prefix=$PT/gmp-$LIBGMP_VER/libgmp-android-$arch
+        $PT/gmp-$LIBGMP_VER/./configure --host="$host" --disable-assembly --enable-cxx --with-pic --prefix=$PT/gmp-$LIBGMP_VER/libgmp-android-$arch
         make
         make install
+        cd $PT/gmp-$LIBGMP_VER || exit
     done
-    cd $PT
+    cd $PT || exit
 fi
 export LIBGMP_PATH=$PT/gmp-$LIBGMP_VER
 
-## MsgPack
+# MsgPack
 echo "MsgPack"
 if [ ! -e "$PT/msgpack-$MSGPACK_VER" ]
 then
@@ -104,18 +112,23 @@ then
     git clone https://github.com/Gherynos/libsaltpack.git
     if [ ! -e "$PT/libsaltpack-$LIBSALTPACK_VER.tar.gz" ]
     then
-        wget -O $PT/libsaltpack-$LIBSALTPACK_VER.tar.gz https://github.com/Gherynos/libsaltpack/archive/v$LIBSALTPACK_VER.tar.gz
+#        wget -O $PT/libsaltpack-$LIBSALTPACK_VER.tar.gz https://github.com/gherynos/libsaltpack/archive/v$LIBSALTPACK_VER.tar.gz
+        wget -O $PT/libsaltpack-$LIBSALTPACK_VER.tar.gz https://github.com/gherynos/libsaltpack/archive/$LIBSALTPACK_VER.tar.gz
     fi
     tar -xvzf libsaltpack-$LIBSALTPACK_VER.tar.gz
-    cd libsaltpack-$LIBSALTPACK_VER/android
+    cd libsaltpack-$LIBSALTPACK_VER || exit
+    mkdir android
+    cp -r $PT/../sp android/jni
+    cd android || exit
     $ANDROID_NDK_HOME/ndk-build
-    cd $PT
+    cd $PT || exit
 fi
 export LIBSALTPACK_PATH=$PT/libsaltpack-$LIBSALTPACK_VER
 
 # LibSaltpack-jni
 echo "LibSaltpack-jni"
-cd /opt/libsaltpack-jni/android
+cd /opt/libsaltpack-jni/android || exit
+printf "\nAPP_PLATFORM := %s\n" $NDK_PLATFORM  >> ./jni/Application.mk
 if [ -e "libsaltpack-jni-libs.jar" ]
 then
     rm -Rf ./lib
@@ -125,4 +138,4 @@ fi
 $ANDROID_NDK_HOME/ndk-build
 mv ./libs ./lib
 zip -r libsaltpack-jni-libs.jar ./lib -x "*.DS_Store"
-cd $PT
+cd $PT || exit
--- android/sp/Android.mk
+++ android/sp/Android.mk
@@ -14,19 +14,7 @@ ifeq ($(ARCH_FOLDER),x86_64)
     ARCH_FOLDER = westmere
 endif
 
-ARCH_FOLDER_LS := $(TARGET_ARCH)
-ifeq ($(ARCH_FOLDER_LS),arm)
-    ARCH_FOLDER_LS = armeabi-v7a
-endif
-ifeq ($(ARCH_FOLDER_LS),arm64)
-    ARCH_FOLDER_LS = arm64-v8a
-endif
-
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := saltpack
-LOCAL_SRC_FILES := ${LIBSALTPACK_PATH}/android/obj/local/$(ARCH_FOLDER_LS)/libsaltpack.a
-include $(PREBUILT_STATIC_LIBRARY)
 
 LOCAL_MODULE := sodium
 LOCAL_SRC_FILES := ${LIBSODIUM_PATH}/libsodium-android-$(ARCH_FOLDER)/lib/libsodium.a
@@ -44,23 +32,22 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := saltpack-jni
+LOCAL_MODULE := saltpack
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../src/main/cpp/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_C_INCLUDES += ${LIBSODIUM_PATH}/libsodium-android-$(ARCH_FOLDER)/include
 LOCAL_C_INCLUDES += ${LIBGMP_PATH}/libgmp-android-$(ARCH_FOLDER)/include
 LOCAL_C_INCLUDES += ${MSGPACK_PATH}/include
-LOCAL_C_INCLUDES += ${LIBSALTPACK_PATH}/include
 
-LOCAL_SRC_FILES += ../../src/main/cpp/InputStreamWrapper.cpp
-LOCAL_SRC_FILES += ../../src/main/cpp/net_nharyes_libsaltpack_MessageReader.cpp
-LOCAL_SRC_FILES += ../../src/main/cpp/net_nharyes_libsaltpack_MessageWriter.cpp
-LOCAL_SRC_FILES += ../../src/main/cpp/net_nharyes_libsaltpack_Utils.cpp
-LOCAL_SRC_FILES += ../../src/main/cpp/OutputStreamWrapper.cpp
+LOCAL_SRC_FILES += ../../src/ArmoredInputStream.cpp
+LOCAL_SRC_FILES += ../../src/ArmoredOutputStream.cpp
+LOCAL_SRC_FILES += ../../src/Base.cpp
+LOCAL_SRC_FILES += ../../src/MessageReader.cpp
+LOCAL_SRC_FILES += ../../src/MessageWriter.cpp
+LOCAL_SRC_FILES += ../../src/Utils.cpp
 
-LOCAL_STATIC_LIBRARIES += saltpack
 LOCAL_STATIC_LIBRARIES += sodium
 LOCAL_STATIC_LIBRARIES += gmp
 LOCAL_STATIC_LIBRARIES += gmpp
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
