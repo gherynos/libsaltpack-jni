2e59c804cb203b02c489718c8d7e425c206b493a
Gherynos <gherynos@naes.co>
2022-10-01 11:50:37 +0100

    Refreshed docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.nharyes.libsaltpack"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.nharyes.libsaltpack"}];updateSearchResults();
